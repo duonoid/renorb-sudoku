09d34e9a11d947e45a82ce5c82a49f4a56e8d70c
David Davis <david.ddavis@gmail.com>
2009-10-15 23:10:39 -0700

    fix: class:Matrix is in ruby stdlib already
    namespace collisons (previous commit)
    
    Looks like a lot of good methods in there... maybe a candidate for
    delegation later.

--- architect.rb
+++ architect.rb
@@ -1,26 +1,28 @@
 #!/usr/bin/ruby
 
-require 'matrix'
+require 'sudoku/matrix'
 
-module Architect
+module Sudoku
+  module Architect
 
-  # Creates a Matrix instance from +io+ stream.
-  #
-  def matrix(io, &blk)
-    matrix = nil
-    io.readlines.each_with_index do |line, idx|
-      chars = line.chars.to_a.delete_if {|d| d.strip.empty? }.compact
-      matrix ||= Matrix.new(chars.length)
-      chars.each_with_index do |char, idx2|
-        cell = matrix.cell(idx2, idx, char)
-        yield cell if block_passed?
+    # Creates a Matrix instance from +io+ stream.
+    #
+    def matrix(io, &blk)
+      matrix = nil
+      io.readlines.each_with_index do |line, idx|
+        chars = line.chars.to_a.delete_if {|d| d.strip.empty? }.compact
+        matrix ||= ::Sudoku::Matrix.new(chars.length)
+        chars.each_with_index do |char, idx2|
+          cell = matrix.cell(idx2, idx, char)
+          yield cell if block_given?
+        end
       end
+
+      matrix
     end
+    module_function :matrix
 
-    matrix
   end
-  module_function :matrix
-
 end
 
 # Architect knows how to construct the Matrix.
@@ -30,7 +32,7 @@ end
 if $0 == __FILE__
   require 'pp'
 
-  matrix = Architect.matrix(STDIN) {|cell| puts cell }
+  matrix = Sudoku::Architect.matrix(STDIN) {|cell| puts cell }
 
   puts "******** matrix:"
   puts matrix
---
+++ sudoku/cell.rb
@@ -0,0 +1,106 @@
+
+module Sudoku
+
+  # Individual 'square' that you write a number in.
+  #
+  class Cell
+
+    attr_accessor :val
+    attr_reader :x, :y, :col, :row, :grid
+    # list of current possibilities for this cell
+    attr_reader :pencils
+    attr_reader :pens # opposite of pencils (known impossibilities)
+
+    # If a +val+ is passed on initialization, consider it immutable.
+    #
+    def initialize(x, y, col, row, grid, val=nil)
+      @x = x; @y = y
+      @col = col; @row = row; @grid = grid
+
+      @col << self
+      @row << self
+      @grid << self
+
+      @val_immutable = false
+      unless val.nil?
+        @val = val
+        @val_immutable = true
+      end
+    end
+
+    # All initial/immutable values are 'valid'.
+    #
+    def valid?
+      @val_immutable or (valid_val? and valid_in_row? and valid_in_col? and valid_in_grid?)
+    end
+
+    def val=(raw)
+      fail if @val_immutable
+      @val = raw
+    end
+
+    def to_s
+      "{x:#{@x}; y:#{@y}; val:#{val}}"
+    end
+
+  private
+
+    def valid_val?
+      @val =~ /\d+/
+    end
+
+    def valid_in_row?
+      uniq_in?(row)
+    end
+
+    def valid_in_col?
+      uniq_in?(col)
+    end
+
+    def valid_in_grid?
+      uniq_in?(grid)
+    end
+
+    def uniq_in?(collection)
+      cnt = collection.count {|c| c.val.to_i > 0 && c.val.to_i == @val.to_i }
+      return true unless cnt > 1
+
+      false
+    end
+
+  end
+
+end
+
+
+if $0 == __FILE__
+  require 'test/unit'
+
+  class TestCell < Test::Unit::TestCase
+    def setup
+      @row = []
+      @col = []
+      @grid = []
+
+      cell1 = Cell.new(0, 0, @col, @row, @grid, 1)
+      @row << cell1
+      @col << cell1
+      @grid << cell1
+    end
+    def test_initial_val
+      @cell = Cell.new(0, 1, @col, @row, @grid, 1234)
+      assert @cell.valid?, "cell is valid when initialized w/ a value"
+    end
+    def test_invalid_val_in_row
+      @cell = Cell.new(0, 1, @col, @row, @grid)
+      @cell.val = 1
+      assert !@cell.__send__(:valid_in_row?)
+    end
+    def test_valid_val_in_row
+      @cell = Cell.new(0, 1, @col, @row, @grid)
+      @cell.val = 2
+      assert @cell.__send__(:valid_in_row?)
+    end
+  end
+
+end
